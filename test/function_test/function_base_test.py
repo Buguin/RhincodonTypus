e57baa43d79c60824d89b7b97be10194e2de06a8
Buguin <wybuguin@hotmail.com>
2017-08-31 00:50:17 +0800

    add pylint check and write a base test

--- test/function_test/function_base_test.py
+++ test/function_test/function_base_test.py
@@ -1,6 +1,24 @@
 # -*- codeing:utf-8 -*-
 # __author__ = 'Buguin'
+"""This is a test for base function of issue track system"""
+import unittest
 from selenium import webdriver
-brower = webdriver.Chrome()
-brower.get("http://localhost:8000")
-assert 'Django' in brower.title
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.brower = webdriver.Chrome()
+        self.brower.implicitly_wait(3)
+
+    def tearDown(self):
+        self.brower.quit()
+
+    def test_web_title(self):
+        self.brower.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.brower.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
