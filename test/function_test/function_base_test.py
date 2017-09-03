c903b7fcdbf98ef505728e8c1149e65b9756a0e9
Buguin <wybuguin@hotmail.com>
2017-09-03 13:57:24 +0800

    complete function test

--- test/function_test/function_base_test.py
+++ test/function_test/function_base_test.py
@@ -2,6 +2,7 @@
 # __author__ = 'Buguin'
 """This is a test for base function of issue track system"""
 import unittest
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 
@@ -18,7 +19,19 @@ class NewVisitorTest(unittest.TestCase):
         self.brower.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.brower.title)
-        # self.fail('Finish the test!')
+        header_text = self.brower.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
+        inputbox = self.brower.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        inputbox.send_keys('Bug peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        table = self.brower.find_elements_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1:Bug peacock feathers' for row in rows))
+
+        self.fail('Finish the test')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
