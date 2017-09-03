1be6208093fdf23e0e1259f383cd9cac44137477
Buguin <wybuguin@hotmail.com>
2017-09-03 14:53:00 +0800

    correct the function test

--- test/function_test/function_base_test.py
+++ test/function_test/function_base_test.py
@@ -9,28 +9,29 @@ from selenium import webdriver
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.brower = webdriver.Chrome()
-        self.brower.implicitly_wait(3)
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        self.brower.quit()
+        self.browser.quit()
 
     def test_web_title(self):
-        self.brower.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000')
 
-        self.assertIn('To-Do', self.brower.title)
-        header_text = self.brower.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.brower.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         inputbox.send_keys('Bug peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
-        table = self.brower.find_elements_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1:Bug peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+                        "New to-do item did not appear in table")
 
         self.fail('Finish the test')
 if __name__ == '__main__':
